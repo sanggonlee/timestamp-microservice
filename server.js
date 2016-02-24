0b587f202ce24a8570b292c5cf88fb4e7b4ce0f2
sanggonlee <lee.sg16@gmail.com>
2016-02-24 16:32:31 +0000

    version 1.0

--- server.js
+++ server.js
@@ -27,6 +27,35 @@ app.use(passport.session());
 
 routes(app, passport);
 
+var monthMapper = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+function convertDate(req, res) {
+	var param = req.url.substr(1);	// strip the first '/' char
+	param = param.replace(/%20/g, " ");
+	var date = isNaN(param) ? new Date(param) : new Date(Number(param));
+
+	return res.end(JSON.stringify({
+		"unix": date.getTime(),
+        "natural": isNaN(date.getTime()) ? null :
+        	monthMapper[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()
+    }));
+}
+
+app.get('/.?*', convertDate);
+
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
